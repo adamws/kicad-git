bbb4f841b062fef114e1b02a41e5a24fe8612e31
adamws <adamws@users.noreply.github.com>
2023-12-22 22:46:27 +0100

    Add plugin package deployment to github pages

---
+++ release/deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -o nounset
+set -o errexit
+
+CUR_DIR="$(dirname $(readlink -f "$0"))"
+cd ${CUR_DIR}/../dist/repository
+
+echo "==> Prepare to deploy\n"
+git init
+git config --global user.name "CircleCI"
+git config --global user.email "${CIRCLE_PROJECT_USERNAME}@users.noreply.github.com"
+
+if [ -z "$(git status --porcelain)" ]; then
+    echo "Something went wrong" && \
+    echo "Exiting..."
+    exit 0
+fi
+
+mkdir .circleci
+wget https://raw.githubusercontent.com/adamws/kicad-git/master/.circleci/ghpages-config.yml -O .circleci/config.yml
+touch .nojekyll
+
+echo "==> Start deploying"
+git add -A
+git commit -m "Deploy plugin repository: ${CIRCLE_SHA1}"
+
+git push --force $CIRCLE_REPOSITORY_URL master:gh-pages
+
+rm -fr .git
+
+echo "==> Deploy succeeded"
