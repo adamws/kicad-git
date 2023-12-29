fbcf4152896688531509307076c2145e642c1459
adamws <adamws@users.noreply.github.com>
2023-12-29 18:51:28 +0100

    Fix class name
    Fixes ruff error-suffix-on-exception-name (N818)

--- source/git_plugin_action.py
+++ source/git_plugin_action.py
@@ -11,7 +11,7 @@ from . import git
 logger = logging.getLogger(__name__)
 
 
-class PluginException(Exception):
+class PluginError(Exception):
     def __init__(self, message) -> None:
         self.message = message
         super().__init__(self.message)
@@ -32,29 +32,29 @@ class GitPluginAction(pcbnew.ActionPlugin):
         version = pcbnew.Version()
         if int(version.split(".")[0]) < 7:
             msg = f"KiCad version {version} is not supported"
-            raise PluginException(msg)
+            raise PluginError(msg)
 
         self.board = pcbnew.GetBoard()
         self.board_file = self.board.GetFileName()
         if not self.board_file:
             msg = "Could not locate .kicad_pcb file, open or create it first"
-            raise PluginException(msg)
+            raise PluginError(msg)
 
         try:
             git_version_str = git.version()
             if not git_version_str.startswith("git version "):
-                msg = "Could not find git executable"
-                raise PluginException(msg)
+                msg = f"Unexpected git version: {git_version_str}"
+                raise PluginError(msg)
         except Exception as e:
             msg = f"Could not find git executable: {e}"
-            raise PluginException(msg)
+            raise PluginError(msg)
 
         self.board_dir = Path(self.board_file).parent
 
         self.repo_dir = git.toplevel(self.board_dir)
         if self.repo_dir is None:
             msg = "Could not locate git repository"
-            raise PluginException(msg)
+            raise PluginError(msg)
 
         # Remove all handlers associated with the root logger object.
         for handler in logging.root.handlers[:]:
@@ -79,7 +79,7 @@ class GitPluginAction(pcbnew.ActionPlugin):
         try:
             self.Initialize()
             initialized = True
-        except PluginException as e:
+        except PluginError as e:
             error = wx.MessageDialog(self.window, e.message, style=wx.ICON_ERROR)
             error.ShowModal()
 
